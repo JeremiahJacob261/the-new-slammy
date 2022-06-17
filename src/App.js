577d0c1ef65122cbceaa590592a401a8ae51e59d
JeremiahJacob261 <jeremiahjacob261@gmail.com>
2022-06-17 22:04:00 +0100

    2 files modified

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./style.css";
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 import Home from './components/Home.js'
+import Product from './components/Product.js'
 
 export default function App() {
   return (
    <Router>
  <Routes>
    <Route path="/" element={<Home/>}/>
+   <Route path="/product" element={<Product/>}/>
  </Routes>
    </Router>
   );
